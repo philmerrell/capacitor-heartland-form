dc4625637de1e93c41f60254abb3f774c81a6a67
Phil Merrell <philmerrell@boisestate.edu>
2018-12-04 21:10:07 -0700

    chore: Add heartlandViewController

---
+++ ios/Plugin/Plugin/HeartlandViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HeartlandViewController.swift
+//  Plugin
+//
+//  Created by Phil Merrell on 12/4/18.
+//  Copyright © 2018 Max Lynch. All rights reserved.
+//
+
+import UIKit
+
+class HeartlandViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
