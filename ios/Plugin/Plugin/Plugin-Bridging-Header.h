bbdcef64f8826229eaa5b9000e902502c6a5e13f
Phil Merrell <philmerrell@boisestate.edu>
2018-12-06 09:23:04 -0700

    Add bridging header for Stripe SDK

---
+++ ios/Plugin/Plugin/Plugin-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Plugin-Bridging-Header.h
+//  Plugin
+//
+//  Created by Phil Merrell on 12/6/18.
+//  Copyright © 2018 Max Lynch. All rights reserved.
+//
+
+#ifndef Plugin_Bridging_Header_h
+#define Plugin_Bridging_Header_h
+
+
+#endif /* Plugin_Bridging_Header_h */
