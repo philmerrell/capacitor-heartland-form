a0b24d8c61cb3f74dcf0b90bcad00c85ed1707f4
Phil Merrell <philmerrell@boisestate.edu>
2018-12-17 11:45:36 -0700

    chore: Add android implementation

--- android/heartland-form/src/main/java/edu/boisestate/plugin/heartland/HeartlandForm.java
+++ android/heartland-form/src/main/java/edu/boisestate/plugin/heartland/HeartlandForm.java
@@ -1,5 +1,7 @@
 package edu.boisestate.plugin.heartland;
 
+import android.content.Intent;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
@@ -16,5 +18,22 @@ public class HeartlandForm extends Plugin {
         JSObject ret = new JSObject();
         ret.put("value", value);
         call.success(ret);
+    }
+
+    @PluginMethod()
+    public void call(PluginCall call) {
+        String publicKey = call.getString("heartlandPublicKey");
+
+        //            We have a key... open the view
+        if (publicKey != null) {
+            presentHeartlandFormView();
+        } else {
+            call.error("You must pass a Heartland public key");
+        }
+    }
+
+    public void presentHeartlandFormView() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        getActivity().startActivity(intent);
     }
 }
